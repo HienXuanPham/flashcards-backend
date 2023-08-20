c98ba730ee696ec925cec579c4a9ed0a5192a828
HienXuanPham <phamxuanhien02@gmail.com>
2023-08-19 21:40:21 -0400

    create Flask application

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flashcards_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
