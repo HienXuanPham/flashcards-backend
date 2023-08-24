97c249fb5247c7446b8bef6683183b66174580fb
HienXuanPham <phamxuanhien02@gmail.com>
2023-08-24 08:42:10 -0400

    read all flashcards or query flashcards by term

--- app/models/flashcard.py
+++ app/models/flashcard.py
@@ -5,3 +5,12 @@ class Flashcard(db.Model):
     term = db.Column(db.Text, nullable=False)
     explanation = db.Column(db.Text, nullable=False)
     
+
+    def convert_to_dict(self):
+        flashcard_dict = {
+            "flashcard_id": self.flashcard_id,
+            "term": self.term,
+            "explanation": self.explanation
+        }
+
+        return flashcard_dict
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from app import db
 from flask import Blueprint, request, jsonify, make_response
 from app.models.flashcard import Flashcard
@@ -13,3 +14,17 @@ def create_one_flashcard():
     db.session.commit()
 
     return make_response(f"Flashcard {new_flashcard.term} successfully created", 201)
+
+@flashcards_bp.route("", methods=["GET"])
+def read_all_flashcards():
+    term_query = request.args.get("term")
+    if term_query:
+        flashcards_query = Flashcard.query.filter(func.lower(Flashcard.term) == term_query.lower())
+    else:
+        flashcards_query = Flashcard.query.all()
+
+    flashcards_response = []
+    for flashcard in flashcards_query:
+        flashcards_response.append(flashcard.convert_to_dict())
+
+    return make_response(jsonify(flashcards_response), 200)
