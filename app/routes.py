45bde700a641b19575ad35c07ee5d696782b7e77
HienXuanPham <phamxuanhien02@gmail.com>
2023-08-25 07:12:00 -0400

    update a flashcard

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
 from sqlalchemy import func
 from app import db
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app.models.flashcard import Flashcard
 
 flashcards_bp = Blueprint("flashcards", __name__, url_prefix="/flashcards")
+
+# Helper function
+def validate_flashcard(cls, flashcard_id):
+    try:
+        flashcard_id = int(flashcard_id)
+    except:
+        abort(make_response({"message": f"{cls.__name__} {flashcard_id} invalid"}, 400))
+
+    flashcard = Flashcard.query.get(flashcard_id)
+
+    if not flashcard:
+        abort(make_response({"message": f"{cls.__name__} {flashcard_id} not found"}, 404))
+
+    return flashcard
 
 @flashcards_bp.route("", methods=["POST"])
 def create_one_flashcard():
@@ -28,3 +42,16 @@ def read_all_flashcards():
         flashcards_response.append(flashcard.convert_to_dict())
 
     return make_response(jsonify(flashcards_response), 200)
+
+@flashcards_bp.route("/<flashcard_id>", methods=["PUT"])
+def update_flashcard(flashcard_id):
+    flashcard = validate_flashcard(Flashcard, flashcard_id)
+
+    request_body = request.get_json()
+    flashcard.term = request_body["term"]
+    flashcard.explanation = request_body["explanation"]
+
+    db.session.add(flashcard)
+    db.session.commit()
+
+    return make_response(jsonify(f"Flashcard {flashcard_id} successfully updated."))
